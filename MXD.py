c4c6e62b674d178780fb36dfe01d27d95749411f
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-01-11 00:25:04 +0400

    Create MXD.py

---
+++ MXD.py
@@ -0,0 +1,11 @@
+import os,time,platform
+os.system('clear')
+print('[>] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import dark
+else:
+    print('\033[1;31m[×] Sorry Device Not Support')
+ 
+ 
